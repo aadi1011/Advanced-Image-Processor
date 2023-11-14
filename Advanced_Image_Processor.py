50f44fc1a22bc8a38140bbc88f2c970a93d4c4f9
Aadith Sukumar <62766172+aadi1011@users.noreply.github.com>
2023-11-14 15:32:21 +0530

    Image Plot functions added

--- Advanced_Image_Processor.py
+++ Advanced_Image_Processor.py
@@ -1,4 +1,5 @@
-# Creating a streamlit app to perform image processing techniques on a given input image
+# Advanced Image Processing Application that performs various techniques on a given input image. 
+# Developer: Aadith Sukumar (https://github.com/aadi1011)
 
 #################### IMPORTS ####################
 
@@ -11,3 +12,19 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import time
 from streamlit_extras.switch_page_button import switch_page 
+
+#################### ALL FUNCTIONS ####################
+
+def plot_image(img):
+    fig = plt.figure(figsize=(10,10))
+    plt.imshow(img, cmap='gray')
+    plt.axis('off')
+    return fig
+
+def plot_histogram(img):
+    fig = plt.figure(figsize=(10,10))
+    plt.hist(img.ravel(), 256, [0,256])
+    plt.title("Histogram")
+    plt.xlabel("Intensity")
+    plt.ylabel("Frequency")
+    return fig
